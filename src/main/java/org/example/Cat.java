ff473919a8bcd8090b3f00fc424cd698d197ee79
Yaroslav <otebator@gmail.com>
2023-10-05 16:10:25 +1000

    Add Test java class cat

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Cat {
+    private Integer age_;
+
+    public int getAge() {
+        return this.age_;
+    }
+
+    private String name_self_;
+
+    public String getName() {
+        return this.name_self_;
+    }
+
+    private String name_owner_;
+
+    public Cat(Integer age,String name_self,String name_owner) {
+        this.age_ = age;
+        this.name_self_ = name_self;
+        this.name_owner_ = name_owner;
+    }
+
+
+    void meow() {
+        if (this.age_ <= 1) {
+            System.out.println("MEEEEEEEEEEOW");
+        }
+        else {
+            System.out.println("MEOW!!!");
+        }
+    }
+
+    void Jump() {
+        System.out.println("Jumped!!");
+    }
+}
