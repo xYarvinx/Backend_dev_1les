d35fd297cd6f597ea26bee9dcac234f16f1a1a4c
Yaroslav <114587634+xYarvinx@users.noreply.github.com>
2023-10-05 16:17:45 +1000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Cat cat_1 = new Cat(10, "Boris", "Nikolay");
+        cat_1.drinkMilk();
+        cat_1.meow();
+        cat_1.jump();
 
+        System.out.println(cat_1.getAge());
+        System.out.println(cat_1.getName());
     }
 }
+
